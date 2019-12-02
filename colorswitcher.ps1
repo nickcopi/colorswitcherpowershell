fc30b3c70715fdc3cabf6c0ba8dc2ba0e7e82b4f
Nick Copi <nickc32200@gmail.com>
2019-12-02 16:24:31 +0000

    Update colorswitcher.ps1

--- colorswitcher.ps1
+++ colorswitcher.ps1
@@ -1,13 +1,16 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
-
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms.Form")  
 
 class Game: System.Windows.Forms.Form{
     static [int32] $SIZE = 500;
     [Square[,]] $grid;
     [System.Windows.Forms.Form] $form;
     [System.Drawing.Graphics] $graphics;
+    [boolean] $won;
     Game(){
+        $this.won = $false;
         $this.initGrid();
         $this.initCanvas();
     }
@@ -22,11 +25,9 @@ class Game: System.Windows.Forms.Form{
         
     }
     [void]initCanvas(){
-        #$this.form = New-Object System.Windows.Forms.Form;
         $this.Text = 'Color Switcher';
         $this.Size = New-Object System.Drawing.Size(([Game]::SIZE+100),([Game]::SIZE+100))
         $this.graphics = $this.CreateGraphics();
-        #$this.form.Add_Load($this.render());
         $this.add_paint({$this.render()});
         $this.add_click({
             $this.handleClick($_);
@@ -34,9 +35,14 @@ class Game: System.Windows.Forms.Form{
         $this.ShowDialog();
     }
     [void]handleClick($event){
+        if($this.won){
+            return;
+        }
         $x = [Math]::Floor($event.x/[Square]::SIZE);
         $y = [Math]::Floor($event.y/[Square]::SIZE);
         $this.doGridClick($x,$y);
+        $this.checkWin();
+        $this.render();
     }
     [void]doGridClick($x,$y){
         if($x-1 -ge 0) {$this.grid[($x-1),$y].toggleSet()};
@@ -44,14 +50,40 @@ class Game: System.Windows.Forms.Form{
         if($x+1 -lt $this.grid.getLength(0)) {$this.grid[($x+1),$y].toggleSet()};
         if($y+1 -lt $this.grid.getLength(1)) {$this.grid[$x,($y+1)].toggleSet()};
         $this.grid[$x,$y].toggleSet();
-        $this.render();
     }
+
+    [void]checkWin(){
+        if($this.isWin()){
+            $this.won = $true;
+        }
+    }
+
+    [boolean]isWin(){
+        $allSet = $true;
+        $allUnset = $true;
+        for($x=0; $x -lt $this.grid.getLength(0); $x++){
+            for($y = 0; $y -lt $this.grid.getLength(1); $y++){
+                    if($this.grid[$x,$y].set){
+                        $allUnset = $false;
+                    } else {
+                        $allSet = $false;
+                    }
+            }
+        }
+        return $allSet -or $allUnset;
+    }
+
     [void]render(){
         $this.graphics.Clear([System.Drawing.Color]::White);
         for($x=0; $x -lt $this.grid.getLength(0); $x++){
             for($y = 0; $y -lt $this.grid.getLength(1); $y++){
                     $this.grid[$x,$y].render($this.graphics);
             }
+        }
+        if($this.won){
+            $brush = New-Object System.Drawing.SolidBrush([System.Drawing.ColorTranslator]::FromHtml('#FFFFFF'));
+            $font = New-Object System.Drawing.Font Consolas,36 
+            $this.graphics.DrawString('You win!',$font,$brush,150,200)
         }
     }
 
@@ -92,4 +124,3 @@ class Square{
 
 }
 $game = New-Object Game
-#[Game]::SIZE
